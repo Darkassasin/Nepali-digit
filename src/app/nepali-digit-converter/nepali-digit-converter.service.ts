ed5abcd9890d269d7382766dd0097115991e2848
Shreeson Shrestha <Darkassasin@users.noreply.github.com>
2018-03-27 10:06:44 +0545

    issues with '0' fixed

--- src/app/nepali-digit-converter/nepali-digit-converter.service.ts
+++ src/app/nepali-digit-converter/nepali-digit-converter.service.ts
@@ -1,27 +1,49 @@
 import {Component, Injectable} from "@angular/core";
 
 @Injectable()
-export class NepaliDigitConverterService{
-    getNepaliDigit(digit):string{
-        if(digit == 0){
-            return '0';
-        }else if(digit ==1 ){
+export class NepaliDigitConverterService {
+    nepaliDigit:string="";
+    convert(entNumber): string {
+        entNumber=String(entNumber);
+        this.nepaliDigit="";
+        let m: number = 0;
+        let totalDigit = entNumber.length;
+        let digitNo = totalDigit;
+        let i = 1;
+        let n: number = parseInt(entNumber);
+        while (i <= digitNo) {
+            m = Math.floor(n / Math.pow(10, totalDigit - 1));
+            // console.log(m);
+            this.nepaliDigit = this.nepaliDigit + this.getNepaliDigit(m);
+            n = n % Math.pow(10, totalDigit - 1);
+            totalDigit -= 1;
+            i++;
+            // console.log(this.nepaliDigit);
+        }
+        console.log(this.nepaliDigit);
+        return this.nepaliDigit;
+    }
+
+    getNepaliDigit(digit): string {
+        if (digit == 0) {
+            return 'o';
+        } else if (digit == 1) {
             return '१';
-        }else if(digit == 2){
+        } else if (digit == 2) {
             return '२';
-        }else if(digit == 3){
+        } else if (digit == 3) {
             return '३';
-        }else if(digit == 4){
+        } else if (digit == 4) {
             return '४';
-        }else if(digit == 5){
+        } else if (digit == 5) {
             return '५';
-        }else if(digit == 6){
+        } else if (digit == 6) {
             return '६';
-        }else if(digit == 7){
+        } else if (digit == 7) {
             return '७';
-        }else if(digit == 8){
+        } else if (digit == 8) {
             return '८';
-        }else if(digit == 9){
+        } else if (digit == 9) {
             return '९';
         }
     }
